64aa3c5d0c4fdd74b2aefdfbdf3135b239a1f47d
mnmhbbb <mnmhb64@gmail.com>
2021-04-27 04:36:02 +0900

    Suspense 로딩 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Switch } from 'react-router';
 import Home from 'pages/Home';
 import PostForm from 'pages/PostForm';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/post" component={PostForm} />
-    </Switch>
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/post" component={PostForm} />
+      </Switch>
+    </Suspense>
   );
 }
 
