4559302f0eca4c9f3e23f0561d15e4563b6f06a9
mnmhbbb <mnmhb64@gmail.com>
2021-04-29 00:27:22 +0900

    배포 전 준비

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,17 @@
 import createSagaMiddleware from 'redux-saga';
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
 import rootReducer from './ducks';
 import rootSaga from './sagas';
+// import logger from 'redux-logger';
 
 const sagaMiddleware = createSagaMiddleware();
 
+// const middleware = process.env.NODE_ENV === 'production' ?
+
 const store = configureStore({
   reducer: rootReducer,
-  middleware: [...getDefaultMiddleware({ thunk: false }), sagaMiddleware, logger],
+  middleware: [...getDefaultMiddleware({ thunk: false }), sagaMiddleware],
+  // middleware: [...getDefaultMiddleware({ thunk: false }), sagaMiddleware, logger],
 });
 
 sagaMiddleware.run(rootSaga);
